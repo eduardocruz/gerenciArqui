61d0359fe6c6f84497cb4c86a62c27ea51bcbf03
Eduardo Cruz <eduardo@eduardocruz.com>
2020-02-18 09:21:12 -0300

    Adding new ending point to list projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Project;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -14,7 +14,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        return Project::all();
     }
 
     /**
